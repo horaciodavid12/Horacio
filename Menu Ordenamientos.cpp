6d5dd43152be8dba1cd0239d34056c90db348171
horaciodavid12 <correahoracio995@gmail.com>
2020-08-11 22:18:52 -0300

    Update Menu Ordenamientos.cpp
    Mejoramiento en la libreria y Metodo burbuja ordenada

--- Menu Ordenamientos.cpp
+++ Menu Ordenamientos.cpp
@@ -4,95 +4,49 @@
 
 
 
-
 main()
 {   
- int opcion, v[100];
-  printf("******************************************************\n");
-  printf("Bienvenidos al Menu de opciones de Ordenamiento\n"); 
-  printf("1) Cargar el Vector\n:");
-  printf("2) Ordenanr burbuja mejorada\n:");
-  printf("3) ordenar por seleccion\n:");
-  printf("4) ordenar por simple\n:");
-  printf("5) ordenar por QuickSort\n:");
-  printf("6) ordenar por MergeSort\n:");
-  printf("7) Salir \n:");
-  scanf("%d", &opcion);
-  printf("***********************************************\n");
+ int opcion, v[1000],i, orden, k[1000];
+  while (opcion!=7)
+  {
+  	
+  
+  	printf("******************************************************\n");
+  	printf("Bienvenidos al Menu de opciones de Ordenamiento\n"); 
+  	printf("1) Cargar el Vector\n:");
+  	printf("2) Ordenar por burbuja mejorada\n:");
+  	printf("3) ordenar por seleccion\n:");
+  	printf("4) ordenar por simple\n:");
+  	printf("5) ordenar por QuickSort\n:");
+  	printf("6) ordenar por MergeSort\n:");
+  	printf("7) Salir \n:");
+  	scanf("%d", &opcion);
+  	printf("***********************************************\n");
   
   switch(opcion)
   {
-  	case 1: cargar ;
-  	int i, v, orden; 	
-	for(i=0; i<orden; i++)	  
-	{
-		printf("ingrese un elemento: ", i+1);
-		scanf("%d", &v[100])
-		  }	  
-		  
-		  
-		  	break;
-  	case 2:  ///burbuja(v);
-  	int array[10];
-  	int i, j, aux, a, lim;
-  	
-  	printf("Digite la cantidad de valores que desea ordenar: ");
-  	scanf("%i", &lim);
-  	llenado del vector
-  	printf("\n");
-  	
-	  for(i=0; i<lim; i++)
-  	{
-  	  printf("valor en [%i]:", i+1);	
-  	  scanf("%i", &array[i]);	
-     }
-  	ordenamiento
-      for(i=0; i<lim; i++)
-  	{
-  	    for(j=0; j<lim; j++)	
-  	   {
-  	       if(array[j] > array[j+1])
-		     { 
-			  aux = array[j];
-			  array[j] = array[j+1];
-			  array[j+1] = aux;
-		     }  
-		}	
-  	}
-  	
-  	printf("\n");
-  	ascendiente
-  	for(i=0; i<lim; i++)
-    {
-  		printf("%i", array[i]);
-  	}
-  	printf("\n\n");
-  	
-  	descendiente
-  	for(i=lim-1; i>=0; i--)
-    {
-  		printf("%i", array[i]);
-  		
-	}
+  	 case 1:  printf("ingrese el tamaño del vector: "); /* carga del vector*/
+			  scanf("%d", &orden);
+			  cargar(orden, v);
+			  break;
+	 case 2:  copiar(orden,v,k); /*ordenamiento burbuja*/
+	  		  burbuja(orden, k); 
+  			  Mostrar(orden, k);
+  			  break;
+  	 case 3:  seleccion(v);
+	  		  break;		
+  	 case 4:  simple(v);
+  			  break;
+     case 5:  QuickSort(v);
+  			  break;
+  	 case 6:  MergeSort(v);
+  			  break;
   	
   	
-  	
-  	
-  			break;
-  	case 3:  seleccion(v);
-	  		break;		
-  	case 4: simple(v);
-  			break;
-  	case 5:  QuickSort(v);
-  			break;
-  	case 6:  MergeSort(v);
-  			break;
-  	case 7:  burbuja(v);
-  	
-  }  
+    }  
 
+   }
 }
-
 
 
 
