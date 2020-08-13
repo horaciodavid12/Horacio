77002959a2d7bbdb9001cc6b34c902c281642744
horaciodavid12 <correahoracio995@gmail.com>
2020-08-12 21:10:53 -0300

    Update Menu Ordenamientos.cpp
    Metodo de Insercion y Seleccion

--- Menu Ordenamientos.cpp
+++ Menu Ordenamientos.cpp
@@ -11,41 +11,48 @@ main()
   {
   	
   
-  	printf("******************************************************\n");
-  	printf("Bienvenidos al Menu de opciones de Ordenamiento\n"); 
-  	printf("1) Cargar el Vector\n:");
-  	printf("2) Ordenar por burbuja mejorada\n:");
-  	printf("3) ordenar por seleccion\n:");
-  	printf("4) ordenar por simple\n:");
-  	printf("5) ordenar por QuickSort\n:");
-  	printf("6) ordenar por MergeSort\n:");
-  	printf("7) Salir \n:");
-  	scanf("%d", &opcion);
-  	printf("***********************************************\n");
+  printf("******************************************************\n");
+  printf("Bienvenidos al Menu de opciones de Ordenamiento\n"); 
+  printf("1) Cargar el Vector\n:");
+  printf("2) Ordenanr burbuja mejorada\n:");
+  printf("3) ordenar por insercion\n:");
+  printf("4) ordenar por seleccion\n:");
+  printf("5) ordenar por QuickSort\n:");
+  printf("6) ordenar por MergeSort\n:");
+  printf("7) Salir \n:");
+  scanf("%d", &opcion);
+  printf("***********************************************\n");
   
   switch(opcion)
   {
-  	 case 1:  printf("ingrese el tamaño del vector: "); /* carga del vector*/
-			  scanf("%d", &orden);
-			  cargar(orden, v);
+  	case 1: printf("ingrese el tamaño del vector"); ///cargar(int orden,int v[1000]); /// carga del vector
+			scanf("%d", &orden);
+			cargar(orden, v);
+			
+			
 			  break;
-	 case 2:  copiar(orden,v,k); /*ordenamiento burbuja*/
-	  		  burbuja(orden, k); 
-  			  Mostrar(orden, k);
-  			  break;
-  	 case 3:  seleccion(v);
-	  		  break;		
-  	 case 4:  simple(v);
-  			  break;
-     case 5:  QuickSort(v);
-  			  break;
-  	 case 6:  MergeSort(v);
-  			  break;
+			  
+  	case 2:  copiar(orden,v,k);
+	  		 burbuja(orden, k); ///ordenamiento burbuja
+  			 Mostrar(orden, v);
+  		 	 break;
+  	case 3: copiar(orden,v, k); ///ordenamiento de insercion
+	  		insercion(orden, v);
+  			Mostrar(orden, v);
+  			 break;	 	 
+  	case 4:  copiar(orden,v, k);  ///ordenamiento de seleccion
+	  		seleccion(orden, v);
+	  		Mostrar(orden, v);
+	  		 break;		
+  	case 5:  QuickSort(orden, v);
+  			 break;
+  	case 6:  MergeSort(v);
+  		 	 break;
   	
   	
-    }  
+   }   
 
-   }
+  }
 }
 
 
