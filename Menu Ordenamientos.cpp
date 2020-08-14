9736e9f2e75f845bbf006fe67fefbd57906d70e7
horaciodavid12 <correahoracio995@gmail.com>
2020-08-13 23:45:29 -0300

    Update Menu Ordenamientos.cpp
    Implementacion de QuickSort

--- Menu Ordenamientos.cpp
+++ Menu Ordenamientos.cpp
@@ -6,14 +6,17 @@
 
 main()
 {   
- int opcion, v[1000],i, orden, k[1000];
+  int salir, carga, t;
+  int opcion, v[1000],i, orden, k[1000];
   while (opcion!=7)
   {
   	
   
-  printf("******************************************************\n");
-  printf("Bienvenidos al Menu de opciones de Ordenamiento\n"); 
-  printf("1) Cargar el Vector\n:");
+  printf("************************************************************\n\n");
+  printf("****      Bienvenidos al Menu de Ordenamientos             *****\n\n"); 
+  printf("****      - - - - - - - - - - - - - - - - - - -            *****\n\n");
+  printf("************************************************************\n\n\n");
+  printf("1) Cargar el Vector: \n");
   printf("2) Ordenanr burbuja mejorada\n:");
   printf("3) ordenar por insercion\n:");
   printf("4) ordenar por seleccion\n:");
@@ -21,35 +24,36 @@ main()
   printf("6) ordenar por MergeSort\n:");
   printf("7) Salir \n:");
   scanf("%d", &opcion);
-  printf("***********************************************\n");
+  
   
   switch(opcion)
   {
-  	case 1: printf("ingrese el tamaño del vector"); ///cargar(int orden,int v[1000]); /// carga del vector
-			scanf("%d", &orden);
+  	case 1: printf("ingrese el tamaño del vector: "); /*carga del vector*/
+  			scanf("%d\n\n ", &orden);
 			cargar(orden, v);
-			
-			
-			  break;
-			  
-  	case 2:  copiar(orden,v,k);
-	  		 burbuja(orden, k); ///ordenamiento burbuja
-  			 Mostrar(orden, v);
+			break;
+	case 2:  copiar(orden,v,k);   /*ordenamiento burbuja mejorada*/
+	  		 burbuja(orden, k); 
+  			 Mostrar(orden, k);
   		 	 break;
-  	case 3: copiar(orden,v, k); ///ordenamiento de insercion
-	  		insercion(orden, v);
+  	case 3: copiar(orden,v, k);   /*ordenamiento de insercion*/
+	  		insercion(orden, k);
   			Mostrar(orden, v);
   			 break;	 	 
-  	case 4:  copiar(orden,v, k);  ///ordenamiento de seleccion
+  	case 4:  copiar(orden,v, k);  /*ordenamiento de seleccion*/
 	  		seleccion(orden, v);
-	  		Mostrar(orden, v);
+	  		Mostrar(orden, k);
 	  		 break;		
-  	case 5:  QuickSort(orden, v);
+  	case 5:  copiar(orden,v,k);   /* Ordenamiento QuickSort*/
+	  		QuickSort(k,0,orden);
+	  		Mostrar(orden, k);
   			 break;
-  	case 6:  MergeSort(v);
-  		 	 break;
-  	
-  	
+  	case 6:  					  /*ordenamiento MergeSort*/
+	  		MergeSort(orden,k);
+	 		 
+  		 	break;
+  	case 7: salir;
+  			printf("Gracias por su visita");
    }   
 
   }
