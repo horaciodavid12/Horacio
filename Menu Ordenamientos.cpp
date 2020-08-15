20798e0502c2c3fba9dbf8d392e7f476cf1e4e36
horaciodavid12 <correahoracio995@gmail.com>
2020-08-15 12:33:15 -0300

    Update Menu Ordenamientos.cpp
    Mejoramiento del programa

--- Menu Ordenamientos.cpp
+++ Menu Ordenamientos.cpp
@@ -1,62 +1,72 @@
 #include<stdio.h>
+#include<conio.h>
 #include<stdlib.h>
 #include"Libreria.h"
 
-
-
 main()
 {   
-  int salir, carga, t;
-  int opcion, v[1000],i, orden, k[1000];
-  while (opcion!=7)
-  {
+  	int salir, carga;
+  	int opcion, v[1000],i, orden, k[1000];
+  
+    printf("\t************************************************************\n\n");
+  	printf("\t****      Bienvenidos al Menu de Ordenamientos         *****\n\n"); 
+  	printf("\t****      - - - - - - - - - - - - - - - - - - -        *****\n\n");
+  	printf("\t************************************************************\n\n\n");
+  
+  
+  	while (opcion<6)
+  	{
+  		printf("\n\n");
+  		printf("\n\tPor favor ingrese el tamaño del vector: ");	/*carga del vector*/
+  		scanf("%d", &orden);
+  		cargar(orden, v);
+  
+  		printf ("\n\n\t- A continuacion podra seleccionar una opcion para ordenar el vector -");
+  
+  		printf("\n\n1) Ordenar burbuja mejorada: \n");
+  		printf("\n2) ordenar por insercion: \n");
+  		printf("\n3) ordenar por seleccion: \n");
+  		printf("\n4) ordenar por QuickSort: \n");
+  		printf("\n5) ordenar por MergeSort: \n");
+  		printf("\n6) Salir: \n");
+  		printf("\n\n\tIngrese una opcion: ");
+  		scanf("%d", &opcion);
+  
+  
+  		switch(opcion)
+  		{
   	
-  
-  printf("************************************************************\n\n");
-  printf("****      Bienvenidos al Menu de Ordenamientos             *****\n\n"); 
-  printf("****      - - - - - - - - - - - - - - - - - - -            *****\n\n");
-  printf("************************************************************\n\n\n");
-  printf("1) Cargar el Vector: \n");
-  printf("2) Ordenanr burbuja mejorada\n:");
-  printf("3) ordenar por insercion\n:");
-  printf("4) ordenar por seleccion\n:");
-  printf("5) ordenar por QuickSort\n:");
-  printf("6) ordenar por MergeSort\n:");
-  printf("7) Salir \n:");
-  scanf("%d", &opcion);
-  
-  
-  switch(opcion)
-  {
-  	case 1: printf("ingrese el tamaño del vector: "); /*carga del vector*/
-  			scanf("%d\n\n ", &orden);
-			cargar(orden, v);
-			break;
-	case 2:  copiar(orden,v,k);   /*ordenamiento burbuja mejorada*/
-	  		 burbuja(orden, k); 
-  			 Mostrar(orden, k);
+		case 1:  copiar(orden,v,k);   /*ordenamiento burbuja mejorada*/
+	  		 burbuja(orden, v); 
+  			 Mostrar(orden, v);
   		 	 break;
-  	case 3: copiar(orden,v, k);   /*ordenamiento de insercion*/
-	  		insercion(orden, k);
+  		case 2: copiar(orden,v, k);   /*ordenamiento de insercion*/
+	  		insercion(orden, v);
   			Mostrar(orden, v);
   			 break;	 	 
-  	case 4:  copiar(orden,v, k);  /*ordenamiento de seleccion*/
+  		case 3:  copiar(orden,v, k);  /*ordenamiento de seleccion*/
 	  		seleccion(orden, v);
-	  		Mostrar(orden, k);
+	  		Mostrar(orden, v);
 	  		 break;		
-  	case 5:  copiar(orden,v,k);   /* Ordenamiento QuickSort*/
+  		case 4:  copiar(orden,v,k);   /* Ordenamiento QuickSort*/
 	  		QuickSort(k,0,orden);
 	  		Mostrar(orden, k);
   			 break;
-  	case 6:  					  /*ordenamiento MergeSort*/
-	  		MergeSort(orden,k);
+  		case 5:  					  /*ordenamiento MergeSort*/
+	  			MergeSort(orden,k);
 	 		 
   		 	break;
-  	case 7: salir;
-  			printf("Gracias por su visita");
+  		case 6: salir;
+  			printf("\n\n");
+  			printf("*********************************************\n\n");
+   			printf("****    Gracias por su visita           *****\n\n");
+   			printf("****    - - - - - - - - - - -            *****\n\n");
+   			printf("*********************************************\n\n");
    }   
 
+
   }
+  printf ("El programa finalizo");
 }
 
 
